895af077a10e7b43d2ed0c8da0812b9e6bdb5274
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-04-26 22:27:06 -0500

    color scheme and nav formatting finalized

---
+++ client/src/pages/AboutTheArtist/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutTheArtist = () => {
+    return (
+        <div>
+            <h1 className='pageTitle'>about the artist:</h1>
+            <img className="aboutImg" alt="Self portrait" src="/images/about/about.png"></img>
+        </div>
+    );
+};
+
+export default AboutTheArtist;
