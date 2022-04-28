ea7ef20a4c2261f146a97b94be8e741ceb7eacaa
I am Billie <dosbor3@gmail.com>
2022-04-27 19:17:04 -0500

    updated quantity property in seeds file to 10K to allow for future use, added image_link property to Product model

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -11,8 +11,8 @@ db.once('open', async () => {
         'hidden bouquet.',
       image: 'img1.jpg',
       image_link: 'https://drive.google.com/file/d/1BD-D2YmOclQFLeNMV-O9GR17y8FXvp6f/view?usp=sharing' ,
-      price: 1.00,
-      quantity: 500
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'Holly Jolly Fall ',
@@ -20,8 +20,8 @@ db.once('open', async () => {
         'Holly Jolly Fall',
       image: 'img2.jpg',
       image_link: 'https://drive.google.com/file/d/1_GfqP2rf3c86LbLSA3YlAuXijPDQceeG/view?usp=sharing',
-      price: 1.00,
-      quantity: 500
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'Summertime Sweater',
@@ -29,8 +29,8 @@ db.once('open', async () => {
         'Summertime Sweater',
       image: 'img3.jpg',
       image_link: 'https://drive.google.com/file/d/1tIVyLtya4-tq1_PyOIz12cneFZo-Jfiu/view?usp=sharing',
-      price: 1.00,
-      quantity: 20
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'inked portrait',
@@ -38,8 +38,8 @@ db.once('open', async () => {
         'inked portrait',
       image: 'img4.jpg',
       image_link: 'https://drive.google.com/file/d/1-G8CYPDtgohBkrupnQTurnO2RMAm9bzF/view?usp=sharing',
-      price: 1.00,
-      quantity: 50
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'spacecatmagdalina',
@@ -47,40 +47,40 @@ db.once('open', async () => {
         'spacecatmagdalina',
       image: 'img5.jpg',
       image_link: 'https://drive.google.com/file/d/1ca4w2o7qK4NUbd1gXOICNnXVmzCcD4dZ/view?usp=sharing',
-      price: 1.00,
-      quantity: 100
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'Girls Who Lift',
       description: 'Girls Who Lift',
       image: 'img6.jpg',
       image_link: 'https://drive.google.com/file/d/1YmtnSCkBzEl4asahMURidFzx-ARE0fYz/view?usp=sharing',
-      price: 1.00,
-      quantity: 30
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'Bubble Tea Bops',
       description: 'Bubble Tea Bops',
       image: 'img7.jpg',
       image_link: 'https://drive.google.com/file/d/1W-cK0KXcszijHtYJUxSdxoBtVZYzjSDP/view?usp=sharing',
-      price: 1.00,
-      quantity: 30
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'Be My Valentine',
       description: 'Be My Valentine',
       image: 'img8.jpg',
       image_link: 'https://drive.google.com/file/d/1_eJi-Br1m8cdalA_H6tvY0IOxJLgQY_i/view?usp=sharing',
-      price: 1.00,
-      quantity: 100
+      price: 5.00,
+      quantity: 10000
     },
     {
       name: 'Fizzy Grape Soda',
       description: 'Fizzy Grape Soda',
       image: 'img9.jpg',
       image_link: 'https://drive.google.com/file/d/1eKGhciENeAp4tvOzXOkReg2iXTv6Mmrx/view?usp=sharing',
-      price: 1.00,
-      quantity: 1000
+      price: 5.00,
+      quantity: 10000
     }
   ]);
 
