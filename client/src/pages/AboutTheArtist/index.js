ea279dc417dc4c7db91ae0e7435eeeb5a8fec0e1
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-04-27 19:14:51 -0500

    component formatting

--- client/src/pages/AboutTheArtist/index.js
+++ client/src/pages/AboutTheArtist/index.js
@@ -4,7 +4,21 @@ const AboutTheArtist = () => {
     return (
         <div>
             <h1 className='pageTitle'>about the artist:</h1>
-            <img className="aboutImg" alt="Self portrait" src="/images/about/about.png"></img>
+            <div id='aboutContainer'>
+                <img className="aboutImg" alt="Self portrait" src="/images/about/about.png"></img>
+                <div id='aboutTextContainer'>
+                    <h2>kirsten michelle</h2>
+                    <div id='aboutTextLine'>
+                        <p><span className='aboutIcon'>🪴</span>texan, coffee lover, 24</p>
+                    </div>
+                    <div id='aboutTextLine'>
+                        <p><span className='aboutIcon'>⭐️</span>digital illustration & character design</p>
+                    </div>
+                    <div id='aboutTextLine'>
+                        <p><span className='aboutIcon'>📸</span>instagram: <a id='instaLink' href='https://www.instagram.com/violetlattes/' target='_blank' rel="noreferrer">@violetlattes</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
