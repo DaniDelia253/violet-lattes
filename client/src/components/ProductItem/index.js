b7d61b8e2f5c9b035f5302c3ea0711dab342e448
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-04-27 19:21:55 -0500

    duplicate prop issue fixed

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -5,7 +5,7 @@ function ProductItem(props) {
 
         <div>
             <div className="card">
-                <img className='productImg' src={props.pic} className="card-img-top" alt='test' />
+                <img className='productImg card-img-top' src={props.pic} alt='test' />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
