745202513a9fc47dac3676df479cd5aa7cfcc64d
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-04-30 10:32:32 -0500

    concurrently set up for client and server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,7 +13,7 @@ const app = express();
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers, 
+    resolvers,
     context: authMiddleware
   });
   await server.start();
@@ -23,6 +25,15 @@ startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
