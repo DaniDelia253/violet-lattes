16e7b806fdb7cf5adde873818d46664ca755acf2
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-05-02 18:20:01 -0500

    heroku fix

--- server/server.js
+++ server/server.js
@@ -29,11 +29,11 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../public/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build'));
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 
