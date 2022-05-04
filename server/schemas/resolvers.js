1dc269a7dc5f8f2df36feafcb2d34b4af1eda962
I am Billie <dosbor3@gmail.com>
2022-05-03 19:32:37 -0500

    updating local repository

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require('stripe')('pk_test_51KvTr7HUATdUTaum0Toi2GWGUb5CkYgYZaeVRWh7x6K23Y9HmZex6szvf40tVuuGBwG1nqHt9pwsPCoVXUl0ovly00WS1Fqphn');
 
 
 const resolvers = {
@@ -36,6 +36,38 @@ const resolvers = {
     checkout: async (parent, args, context) => {
       const order = new Order({ products: args.products });
       const { products } = await order.populate('products');
+
+      const line_items = [];
+
+      for (let i = 0; i < products.length; i++) { 
+        // generate product id
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description
+        });
+
+        // generate price id using the product id
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        // add price id to the line items array
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel'
+      });
+      
+      return { session: session.id };
     },
     order: async (parent, { _id }, context) => {
       if (context.user) {
